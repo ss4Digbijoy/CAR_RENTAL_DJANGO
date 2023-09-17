2e303773e1d8881e61169957d85b6d49cd0d530e
ss4Digbijoy <ss4digbijoydutta@gmail.com>
2023-09-18 01:09:04 +0530

    changes for dployment 4

--- my_tennis_game/settings.py
+++ my_tennis_game/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['car-rental-django-by-digbijoy-git-main-ss4digbijoy.vercel.app']
 
 
 # Application definition
